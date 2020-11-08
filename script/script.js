c4427c4ba2a85b157a7c9d11857b3257d2351112
SaltyBr <SaltyBrain@yandex.ru>
2020-11-08 19:06:17 +0300

    lesson18 main task done

--- script/script.js
+++ script/script.js
@@ -3,12 +3,49 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //таймер
     function countTimer(deadline) {
-        const hours = document.querySelector('#timer-hours'),
-            minutes = document.querySelector('#timer-minutes'),
-            seconds = document.querySelector('#timer-seconds'),
-            dateStop = new Date(deadline).getTime,
-            dateNow = new Date().getTime;
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+
+        function getTimeRemaining(){
+            let dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime();
+            timeRemaining = (dateStop - dateNow) / 1000;
+            seconds = Math.floor(timeRemaining % 60);
+            minutes = Math.floor((timeRemaining / 60) % 60);
+            hours = Math.floor((timeRemaining / 3600));
+            return {timeRemaining, hours, minutes, seconds}
+        }
+        
+        const addZero = ((a)=>{
+            if (a < 10 && a >= 0){
+                a = '0' + a;
+            }
+            return a
+        });
+        
+        function updateClock(){
+            let timer = getTimeRemaining(),
+                idInterval;
+    
+            if (timeRemaining > 0){
+                timerHours.textContent = addZero(timer.hours);
+                timerMinutes.textContent = addZero(timer.minutes);
+                timerSeconds.textContent = addZero(timer.seconds);
+                idInterval = setInterval(updateClock, 1000)}
+            else{
+                timerHours.textContent = '00'; 
+                timerMinutes.textContent = '00'; 
+                timerSeconds.textContent = '00';   
+            }
+        }
+
+        updateClock();
+            
+
     }
 
-    countTimer('01 july 2019');
+    countTimer('9 nov 2020');
 });
+
