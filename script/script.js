cca3397ba1c7a864853d1cdb70b19bf799916525
SaltyBr <SaltyBrain@yandex.ru>
2020-11-08 21:46:33 +0300

    added jshint

--- script/script.js
+++ script/script.js
@@ -10,30 +10,30 @@ window.addEventListener('DOMContentLoaded', () => {
 
         function getTimeRemaining(){
             let dateStop = new Date(deadline).getTime(),
-            dateNow = new Date().getTime();
-            timeRemaining = (dateStop - dateNow) / 1000;
-            seconds = Math.floor(timeRemaining % 60);
-            minutes = Math.floor((timeRemaining / 60) % 60);
-            hours = Math.floor((timeRemaining / 3600));
-            return {timeRemaining, hours, minutes, seconds}
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor((timeRemaining / 3600));
+            return {timeRemaining, hours, minutes, seconds};
         }
         
         const addZero = ((a)=>{
             if (a < 10 && a >= 0){
                 a = '0' + a;
             }
-            return a
+            return a;
         });
         
         function updateClock(){
             let timer = getTimeRemaining(),
                 idInterval;
     
-            if (timeRemaining > 0){
+            if ( timeRemaining > 0 ){
                 timerHours.textContent = addZero(timer.hours);
                 timerMinutes.textContent = addZero(timer.minutes);
                 timerSeconds.textContent = addZero(timer.seconds);
-                idInterval = setInterval(updateClock, 1000)}
+                idInterval = setInterval(updateClock, 1000);}
             else{
                 timerHours.textContent = '00'; 
                 timerMinutes.textContent = '00'; 
