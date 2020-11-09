e9d7bfe48d6269efb639a5a47fe4d789b5af7a8f
SaltyBr <SaltyBrain@yandex.ru>
2020-11-09 21:01:47 +0300

    lesson19 main and add task done

--- script/script.js
+++ script/script.js
@@ -1,7 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    function animate({timing, draw, duration}) { // паттерн анимации
 
-    //таймер
+        let start = performance.now();
+      
+        requestAnimationFrame(function animate(time) {
+          // timeFraction изменяется от 0 до 1
+          let timeFraction = (time - start) / duration;
+          if (timeFraction > 1) {timeFraction = 1;}
+      
+          // вычисление текущего состояния анимации
+          let progress = timing(timeFraction);
+      
+          draw(progress); // отрисовать её
+      
+          if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+          }
+      
+        });
+    }
+
+    //timer
     function countTimer(deadline) {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
@@ -26,26 +46,92 @@ window.addEventListener('DOMContentLoaded', () => {
         });
         
         function updateClock(){
-            let timer = getTimeRemaining(),
-                idInterval;
+            let timer = getTimeRemaining();
     
             if ( timer.timeRemaining > 0 ){
                 timerHours.textContent = addZero(timer.hours);
                 timerMinutes.textContent = addZero(timer.minutes);
                 timerSeconds.textContent = addZero(timer.seconds);
-                idInterval = setInterval(updateClock, 1000);}
+            }
             else{
                 timerHours.textContent = '00'; 
                 timerMinutes.textContent = '00'; 
                 timerSeconds.textContent = '00';   
             }
         }
-
         updateClock();
+        setInterval(updateClock, 1000);
+
             
 
     }
 
-    countTimer('10 nov 2020');
+    countTimer('11 nov 2020');
+
+    //menu
+    const toggleMenu = ()=>{
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItem = menu.querySelectorAll('ul>li'),
+            anchors = document.querySelectorAll('a[href^="#"]');
+
+            const handlerMenu = ()=>{
+                menu.classList.toggle('active-menu');
+            };
+
+        btnMenu.addEventListener('click', handlerMenu);
+
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItem.forEach((item) => item.addEventListener('click', handlerMenu));
+
+        anchors.forEach((item)=>{ // плавная прокрутка по якорям
+            item.addEventListener('click', (event)=>{
+                event.preventDefault();
+                const blockId = item.getAttribute('href');
+                document.querySelector('' + blockId).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        });
+    };
+    toggleMenu();
+
+    // popup
+    const togglePopup = () =>{
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content'),
+            docElem = document.documentElement;
+
+
+        popupBtn.forEach((item)=>{
+            item.addEventListener('click', ()=>{
+                if (docElem.scrollWidth < 769){
+                popup.style.display = 'block';
+                } else {
+                    popup.style.display = 'block';
+                    animate({
+                        duration: 600,
+                        timing(timeFraction) {
+                            return 1 - Math.sin(Math.acos(timeFraction));
+                        },
+                        draw(progress) {
+                            popupContent.style.opacity = progress;
+                        }
+                    });
+                }
+            });
+        });
+
+        popupClose.addEventListener('click', ()=>{
+            popup.style.display = 'none';
+        });
+    };
+    togglePopup();
+
 });
 
