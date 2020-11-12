9129f7ece9301d269dfd9b9480a7412499c49801
SaltyBr <SaltyBrain@yandex.ru>
2020-11-12 19:51:02 +0300

    add one eventListener

--- script/script.js
+++ script/script.js
@@ -70,28 +70,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //menu
 const toggleMenu = ()=>{
-	const menu = document.querySelectorAll('menu, .menu');
-
-	const handlerMenu = (item)=>{
-			item.classList.toggle('active-menu');
+	const menu = document.querySelector('menu'),
+				menuBtn = document.querySelector('.menu'),
+				closeBtn = document.querySelector('.close-btn'),
+				serviceBtn = document.querySelector('main>a');
+		
+	const handlerMenu = ()=>{
+		menu.classList.toggle('active-menu');
 	};
 
-	menu.forEach((item, i)=>{
-		item.addEventListener('click', (event)=>{
-			let target = event.target;
-			if(target){
-				handlerMenu(document.querySelector('menu'));
-			}
-			if(target.matches('a[href^="#"]')){
-				event.preventDefault();
-				const blockId = target.getAttribute('href');
-				if(blockId !== '#close'){
-				document.querySelector(`${blockId}`).scrollIntoView({
-					behavior: 'smooth',
-					block: 'start'
-				});}
-			}
-		});
+	document.addEventListener('click', (event)=>{
+		let target = event.target;
+		if(target.closest('.menu') === menuBtn || target === closeBtn){
+			handlerMenu();
+		}else if(target.matches('a[href^="#"]')){
+			event.preventDefault();
+			const blockId = target.getAttribute('href');
+			if(blockId !== '#close' && blockId.length > 2){
+			document.querySelector(`${blockId}`).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+			handlerMenu();
+		}
+		}else if (target.closest('main>a') === serviceBtn){
+			event.preventDefault();
+			const blockId = target.closest('main>a').getAttribute('href');
+ 			if(blockId !== '#close'){
+			document.querySelector(`${blockId}`).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});}
+		}
 	});
 };
 toggleMenu();
