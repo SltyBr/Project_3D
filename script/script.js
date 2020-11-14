2be48526ea3d804d362469f30e3e6a9fe8ef836c
SaltyBr <SaltyBrain@yandex.ru>
2020-11-14 22:52:34 +0300

    lesson24 main and add task done

--- script/script.js
+++ script/script.js
@@ -298,18 +298,66 @@ toggleMenu();
 	};
 	changePhoto();
 
-	// ввод цифр в калькулятор
-	const calcDigitsOnly = ()=>{
-		const calc = document.querySelector('.calc'),
-					calcInputs = calc.querySelectorAll('input');
+	// калькулятор
+	const calc = (price = 100)=>{
 
-		calcInputs.forEach((item)=>{
-			item.addEventListener('input', ()=>{
-				item.value = item.value.replace(/\D/g, '');
+		const calcBlock = document.querySelector('.calc-block'),
+					calcType = document.querySelector('.calc-type'),
+					calcSquare = document.querySelector('.calc-square'),
+					calcCount = document.querySelector('.calc-count'),
+					calcDay = document.querySelector('.calc-day'),
+					totalValue = document.getElementById('total');
+
+		const calcDigitsOnly = ()=>{ // функция, которая пропускает только цифры
+			const calc = document.querySelector('.calc'),
+						calcInputs = calc.querySelectorAll('input');
+	
+			calcInputs.forEach((item)=>{
+				item.addEventListener('input', ()=>{
+					item.value = item.value.replace(/\D/g, '');
+				});
 			});
+		};
+		calcDigitsOnly();
+
+		const countSum = ()=>{
+			let total = 0,
+					countValue = 1,
+					dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value,
+						squareValue = +calcSquare.value;
+
+				if(calcCount.value > 1){
+					countValue += (calcCount.value - 1) / 10;
+				}
+
+				if(calcDay.value < 5 && calcDay.value){
+					dayValue *= 2;
+				} else if(calcDay.value < 10 && calcDay.value){
+					dayValue *= 1.5;
+				}
+
+				if(typeValue && squareValue){
+					total = price * typeValue * squareValue * countValue * dayValue;
+				}
+				animate({
+					duration: 300,
+					timing(timeFraction) {
+						return timeFraction;
+					},
+					draw(progress) {
+						totalValue.textContent = Math.ceil(progress*total);
+					}
+				});				
+		};
+
+		calcBlock.addEventListener('change', (event)=>{
+			const target = event.target;
+			if((target === calcType || target === calcSquare || target === calcCount || target === calcDay)){
+				countSum();
+			}
 		});
 	};
-	calcDigitsOnly();
-
+	calc();
 });
 
