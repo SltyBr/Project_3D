ab041b5b48f36e909fb25cf4a9adc1374d37701e
SaltyBr <SaltyBrain@yandex.ru>
2020-11-16 18:02:33 +0300

    добавил аякс в нужную ветку

--- script/script.js
+++ script/script.js
@@ -351,4 +351,78 @@ toggleMenu();
 		});
 	};
 	calc();
+
+		//send-ajax=form
+
+		const sendForm = (formId)=>{
+			const errorMessage = 'Что-то пошло не так',
+					loadMessage = 'Загрузка...',
+					successMessage = 'Мы скоро с Вами свяжемся!';
+	
+			const form = document.getElementById(`${formId}`),
+						formInputs = form.querySelectorAll('input');
+	
+			const statusMessage = document.createElement('div');
+			statusMessage.style.cssText = 'font-size: 2rem; color: #19b5fe';
+	
+			formInputs.forEach((item)=>{
+				item.addEventListener('input', ()=>{
+				if (item.classList.contains('form-phone')){
+					item.value = item.value.replace (/[^0-9+]/, '');
+				}
+				 if (item.classList.contains('form-name') || item.classList.contains('mess')){
+					item.value = item.value.replace(/[^а-я ]/gi, '');
+				}
+				});
+			});
+	
+			form.addEventListener('submit', (event)=>{
+				event.preventDefault();
+				form.append(statusMessage);
+				statusMessage.textContent = loadMessage;
+	
+				const formData = new FormData(form);
+				let body = {};
+	
+				formData.forEach((val, key)=>{
+					body[key] = val;
+				});
+				postData(body, ()=>{
+					statusMessage.textContent = successMessage;
+					
+					formInputs.forEach((item)=>{
+						item.value = '';
+					});
+				}, (error) =>{
+					statusMessage.textContent = errorMessage;
+					console.error(error);
+				});
+	
+			});
+	
+			const postData = (body, outputData, errorData)=>{
+				const request = new XMLHttpRequest();
+				request.addEventListener('readystatechange', ()=>{
+					if(request.readyState !== 4){
+						return;
+					}
+	
+					if(request.status === 200){
+						outputData();
+					} else{
+						errorData(request.status);
+					}
+				});
+				request.open('POST', './server.php');
+				request.setRequestHeader('Content-Type', 'application/json');
+	
+				request.send(JSON.stringify(body));
+	
+			};
+	
+	
+		};
+		sendForm('form1');
+		sendForm('form2');
+		sendForm('form3');
 });
